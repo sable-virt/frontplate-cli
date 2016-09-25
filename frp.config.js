b708a49eca83d85381ba17c7067188274769be10
frontainer <frontainer@gmail.com>
2016-09-25 20:28:41 +0900

    configファイル名リネームに合わせて修正

--- frp.config.js
+++ frp.config.js
@@ -6,11 +6,11 @@ module.exports = function(production) {
         },
         html: require('./config/html.config'),
         image: require('./config/image.config'),
-        test: require('./config/karma.conf'),
+        test: require('./config/test.conf'),
         style: require('./config/style.config'),
         script: production ? require('./config/webpack.config.production') : require('./config/webpack.config'),
         server: require('./config/server.config'),
-        copy: require('./config/sync.config'),
+        copy: require('./config/copy.config'),
         sprite: require('./config/sprite.config')
     }
 };
