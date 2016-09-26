d4b85bcb776439338665c011622673f4a8177927
frontainer <frontainer@gmail.com>
2016-09-26 16:58:28 +0900

    historyApiFallbackはデフォルトサポートしない

--- config/server.config.js
+++ config/server.config.js
@@ -1,10 +1,8 @@
 'use strict';
-const historyApiFallback = require('connect-history-api-fallback');
 module.exports = {
   "server": 'public',
   "port": 3000,
   "middleware": [
-    // historyApiFallback()
   ],
   "ghostMode": {
     "clicks": true,
