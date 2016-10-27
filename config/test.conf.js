c94a559bc000b7d2b4f7fc94ec2a27899c0f6315
frontainer <frontainer@gmail.com>
2016-10-27 15:08:46 +0900

    breaking: test関連の設定ファイルを通常のオブジェクトに変更

--- config/test.conf.js
+++ config/test.conf.js
@@ -1,58 +1,63 @@
 'use strict';
 /**
  * karma conf
- * @param config
  * url: https://karma-runner.github.io/1.0/config/configuration-file.html
  */
-module.exports = function (config) {
-    config.set({
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            'src/js/**/*spec.js'
-        ],
-        preprocessors: {
-            'src/js/**/*spec.js': ['webpack','babel']
-        },
-        webpack: {
-            resolve: { modules: [ './src/js'] },
-        },
-        webpackMiddleware: {
-            quiet: true,
-            stats: 'errors-only'
-        },
-        exclude: [],
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
-        client: {
-            captureConsole: true
-        },
-        // web server port
-        port: 9001,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+module.exports = {
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '.',
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // list of files / patterns to load in the browser
+    files: [
+        'src/js/**/*spec.js'
+    ],
+    preprocessors: {
+        '**/*spec.js': ['webpack','babel']
+    },
+    webpack: {
+        resolve: { modules: [ './src/js'] },
+    },
+    webpackMiddleware: {
+        quiet: true,
+        stats: 'errors-only'
+    },
+    babelPreprocessor: {
+        options: {
+            presets: ['es2015'],
+            sourceMap: 'inline'
+        }
+    },
+    exclude: [],
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+    client: {
+        captureConsole: true
+    },
+    // web server port
+    port: 9001,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+    // level of logging
+    //'OFF' | 'ERROR' | 'INFO' | 'DEBUG'
+    logLevel: 'INFO',
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true
-    });
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
 };
