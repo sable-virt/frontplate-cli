e89c3a8d975a74421c879753669f4d7322ced11e
frontainer <frontainer@gmail.com>
2016-12-02 15:38:55 +0900

    new: Retina対応したSpriteを生成できるように #13

--- config/sprite.config.js
+++ config/sprite.config.js
@@ -5,6 +5,9 @@ module.exports = [
         destImage: `${FRP_SRC}/images/icon.png`,           // 画像出力先
         destCSS: `${FRP_SRC}/sass/sprites/_icon.scss`,     // CSS出力先
         imgPath: '../images/icon.png',              // CSS内での画像パス
-        padding: 2                                  // 画像の間隔
+        padding: 2,                                 // 画像の間隔
+        scale: 1.0,                                 // 画像表示サイズ（Retina対応なら0.5にする）
+        mixin: true,                               // @mixin spriteを出力するかどうか
+        selector: true                              // .icon {&.icon-name {}}のクラス定義を出力するかどうか
     }
 ];
