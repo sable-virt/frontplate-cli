5b1be1ee392d1597266ce03b692cf07d776e60c7
frontainer <frontainer@gmail.com>
2016-12-26 19:33:47 +0900

    fix: ファイルサイズが250KBを超えたときのwebpackのwarningを非表示
    ```
    WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit (250 kB). This can impact web performance.
    ```

--- config/webpack.core.js
+++ config/webpack.core.js
@@ -3,57 +3,60 @@ const webpack = require("webpack");
 const entries = require("webpack-entries");
 const path = require("path");
 const util = require("../lib/util/util");
-const localConfig = path.join(process.cwd(),'.eslintrc');
-const globalConfig = path.join(__dirname,'/../.eslintrc');
+const localConfig = path.join(process.cwd(), '.eslintrc');
+const globalConfig = path.join(__dirname, '/../.eslintrc');
 /**
  * webpack config
  * url: https://webpack.github.io/docs/configuration.html
  */
 module.exports = {
-    entry: entries(`./${FRP_SRC}/js/!(_*|*spec).js`,true),
-    output: {
-        path: FRP_DEST + '/assets/js',
-        publicPath: '/assets/js/',
-        filename: "[name].js",
-        sourceMapFilename: 'maps/[name].map',
-        jsonpFunction: 'fr'
-    },
-    resolve: {
-        modules: [
-            `${FRP_SRC}/js`,
-            path.join(process.cwd(), 'node_modules'),
-            path.join(__dirname, '../node_modules'),
-            "node_modules"
-        ],
-    },
-    resolveLoader: {
-        modules: [
-            path.join(process.cwd(),'node_modules'),
-            path.join(__dirname, '../node_modules'),
-            "node_modules"
-        ]
-    },
-    module: {
-        rules: [
-            {test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader', enforce: 'pre'},
-            {test: /\.html$/, loader: 'html-loader'},
-            {test: /\.json$/, loader: 'json-loader'},
-        ]
-    },
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                eslint: {
-                    configFile: util.exists(localConfig) ? localConfig : globalConfig,
-                    failOnError: true
-                },
-                babel: {
-                    presets: ["es2015"]
-                }
-            }
-        })
+  entry: entries(`./${FRP_SRC}/js/!(_*|*spec).js`, true),
+  output: {
+    path: FRP_DEST + '/assets/js',
+    publicPath: '/assets/js/',
+    filename: "[name].js",
+    sourceMapFilename: 'maps/[name].map',
+    jsonpFunction: 'fr'
+  },
+  resolve: {
+    modules: [
+      `${FRP_SRC}/js`,
+      path.join(process.cwd(), 'node_modules'),
+      path.join(__dirname, '../node_modules'),
+      "node_modules"
     ],
-    watchOptions: {
-        ignored: /node_modules/
-    }
+  },
+  resolveLoader: {
+    modules: [
+      path.join(process.cwd(), 'node_modules'),
+      path.join(__dirname, '../node_modules'),
+      "node_modules"
+    ]
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader', enforce: 'pre'},
+      {test: /\.html$/, loader: 'html-loader'},
+      {test: /\.json$/, loader: 'json-loader'},
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        eslint: {
+          configFile: util.exists(localConfig) ? localConfig : globalConfig,
+          failOnError: true
+        },
+        babel: {
+          presets: ["es2015"]
+        }
+      }
+    })
+  ],
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  performance: {
+    hints: false
+  }
 };
