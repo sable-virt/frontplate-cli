e196d21681f201478316a85d93298dd1a55f901d
frontainer <frontainer@gmail.com>
2017-01-11 17:25:38 +0900

    微調整

--- wp/build/common.js
+++ wp/build/common.js
@@ -22,37 +22,5 @@ module.exports = {
   },
   performance: {
     hints: false
-  },
-  stats: {
-    // Add asset Information
-    assets: true,
-    // Add information about cached (not built) modules
-    cached: true,
-    // Add children information
-    children: false,
-    // Add chunk information (setting this to `false` allows for a less verbose output)
-    chunks: false,
-    // Add built modules information to chunk information
-    chunkModules: false,
-    // Add the origins of chunks and chunk merging info
-    chunkOrigins: false,
-    // Add errors
-    errors: true,
-    // Add details to errors (like resolving log)
-    errorDetails: true,
-    // Add the hash of the compilation
-    hash: false,
-    // Add built modules information
-    modules: false,
-    // Add information about the reasons why modules are included
-    reasons: true,
-    // Add the source code of modules
-    source: false,
-    // Add timing information
-    timings: true,
-    // Add webpack version information
-    version: false,
-    // Add warnings
-    warnings: true
   }
 };
